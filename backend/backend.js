3912ca649e3ce1ad2a2d83d873b6d6172aa77e4d
Felix <felix@felixgeelhaar.de>
2014-10-25 17:25:30 +0200

    Creating url function

--- backend/backend.js
+++ backend/backend.js
@@ -3,39 +3,77 @@
  */
 
 /**
+ * Requirements for database and node.js
+ * @type {exports}
+ */
+var http = require('http');
+var mongoose = require('mongoose');
+
+/**
+ * TODO Build up the url with given parameters
+ */
+var url = 'http://api.regenesis.pudo.org/cube/';
+
+tc = '71231gj001';
+y = '2008';
+
+var createURL = function(tc, y, rs, fn){
+   url += tc + '/' + 'aggregate?cut=jahr.text:' + y;
+};
+
+http://api.regenesis.pudo.org/cube/71231gj001/aggregate?cut=jahr.text:2008&drilldown=gemein
+
+/**
  * TODO Connecting with mongoDB
  */
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var connect = mongoose.connect('mongodb://localhost/checkgermany');
+if(connect)
+    console.log('connected');
+else
+    console.log('not connected');
 
 /**
- * TODO Get data from regenesis server
- */
-http.get("http://api.regenesis.pudo.org/model", function(data) {
-    console.log("Got response: " + data.statusCode);
+ * Get data from regenesis server
+*/
+
+http.get(url, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    res.on('end', function() {
+       var response = JSON.parse(body);
+        console.log("Got response: ", response);
+    });
 }).on('error', function(e) {
-    console.log("Got error: " + e.message);
+    console.log("Got error: ", e);
 });
 
 /**
- * TODO Save data set into table
- */
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-    if (err) // ...
-        console.log('meow');
-});
-//TODO solange daten in datenset; sichere diese in datenbank
-
-/**
- * Update data set in table
+ * TODO Creating an JSON Object of data
  */
 
-// TODO prüfe ob daten aktuell; wenn nicht aktualisiere diese
 
-/**
- * Create table for data
- */
-var Cat = mongoose.model('Cat', { name: String });
-// TODO erstelle tabelle für daten;
+///**
+// * TODO Save data set into table
+// */
+//var kitty = new Cat({ name: 'Zildjian' });
+//kitty.save(function (err) {
+//    if (err) // ...
+//        console.log('meow');
+//});
+////TODO solange daten in datenset; sichere diese in datenbank
+//
+///**
+// * Update data set in table
+// */
+//
+//// TODO prüfe ob daten aktuell; wenn nicht aktualisiere diese
+//
+///**
+// * Create table for data
+// */
+//var Cat = mongoose.model('Cat', { name: String });
+//// TODO erstelle tabelle für daten;
 
