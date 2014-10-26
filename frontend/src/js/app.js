11c58bfb1b7c96576e67a919d96c182675281828
Felix Ebert <felix.ebert@if-core.de>
2014-10-26 05:27:52 +0100

    add angular loading bar

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -2,7 +2,7 @@ var app = {};
 (function (angular) {
     'use strict';
     angular.module('app.explore', []);
-    angular.module('app', ['ngRoute', 'app.explore']).config(function ($routeProvider, $locationProvider) {
+    angular.module('app', ['ngRoute', 'angular-loading-bar', 'app.explore']).config(function ($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
         $routeProvider
             .when('/', {
