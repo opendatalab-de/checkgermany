a6c9a4ab3e38a9625314f3f295b58aa54f28d427
Felix Ebert <felix.ebert@if-core.de>
2014-12-23 18:27:27 +0100

    refactoring: remove bootstrap, jquery and ng-route dependency add normalize.css and ngDialog

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -2,12 +2,5 @@ var app = {};
 (function (angular) {
     'use strict';
     angular.module('app.explore', []);
-    angular.module('app', ['ngRoute', 'angular-loading-bar', 'app.explore']).config(function ($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider
-            .when('/', {
-                controller: 'ExploreCtrl',
-                templateUrl: 'partials/explore.html'
-            });
-    });
+    angular.module('app', ['angular-loading-bar', 'ngDialog', 'app.explore']);
 })(angular);
